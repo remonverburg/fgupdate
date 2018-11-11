89e1b1bff804d5bc54fa1cc571a5825671cb9e3e
remonverburg <remonverburg100@hotmail.com>
2018-11-11 11:10:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,68 +1,226 @@
 #!/bin/bash
 clear
+#aaaa
+##bash <( curl https://raw.githubusercontent.com/ktjbrowne/FGC-MN-Install/master/install.sh )
+# Set these to change the version of FantasyGold to install
+TARBALLURL="https://github.com/remonverburg/fgupdate/releases/download/127/127fgc.tar.gz"
+TARBALLNAME="127fgc.tar.gz"
+FGCVERSION="1.2.7"
+BOOTSTRAPURL="https://github.com/FantasyGold/FantasyGold-Core/releases/download/v1.2.6/FGC-Bootstrap-1.2.6.tar.gz"
+BOOTSTRAPFILE="FGC-Bootstrap-1.2.6.tar.gz"
 
-TARBALLURL="https://github.com/FantasyGold/FantasyGold-Core/releases/download/v1.2.5/FantasyGold-1.2.5-Linux-x64.tar.gz"
-TARBALLNAME="FantasyGold-1.2.5-Linux-x64.tar.gz"
-FGCVERSION="1.2.5"
+SPINNER="/-\\|"
+##############################################################################
+##Functions
+printHead0() {
+  printf "\\n\\n\\e[43;30m***    %-30s    ***\\e[0m\\n" "$1"
+}
 
-CHARS="/-\|"
+printHead1() {
+  printf "\\e[96;40m* %-30s *\\e[0m\\n" "$1"
+}
 
-clear
+waitOnProgram() {
+  local MESSAGE=$1
+  local PID=$!
+  local i=1
+  while [ -d /proc/$PID ]; do
+    printf "\\e[96;40m\\r${SPINNER:i++%${#SPINNER}:1} ${MESSAGE}\\e[0m"
+    sleep 0.3
+  done
+  echo
+}
+#############################################################################
 echo "
- +----------------------------------------------------script.v1.2+::
- | FantasyGold Masternode Update Script Version: $FGCVERSION           |::
- |                                                               |::
- | This script is complemintary to the original install script.  |::
- | If you manually installed, please update your VPS manually.   |::
- |                                                               |::
- +---------------------------------------------------------------+::
+ :::::::::::::::::::::::::::::::::::::::::::::::::::
+ +-------------------------------------------------+
+            __________________________
+            ___  ____/_  ____/_  ____/
+            __  /_   _  / __ _  /
+            _  __/   / /_/ / / /___
+            /_/      \____/  \____/ v1.2.6
+ +-------------------------------------------------+
+ +-------  FGC MASTERNODE INSTALLER v1.2.6  -------+
+ :::::::::::::::::::::::::::::::::::::::::::::::::::
 "
-read -p "Press Ctrl-C to abort or any other key to continue. " -n1 -s
-clear
+sleep 3
 
+printHead0 "Performing System Checks"
+sleep 1
+# Check if we are root
 if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root."
+   printHead1 "This script must be run as root." 1>&2
+   exit 1
+fi
+
+# Check if we have enough memory
+if [[ `free -m | awk '/^Mem:/{print $2}'` -lt 900 ]]; then
+  printHead1 "This installation requires at least 1GB of RAM.";
   exit 1
 fi
 
-#USER=`ps u $(pgrep fantasygoldd) | grep fantasygoldd | cut -d " " -f 1`
+# Check if we have enough disk space
+if [[ `df -k --output=avail / | tail -n1` -lt 10485760 ]]; then
+  printHead1 "This installation requires at least 10GB of free disk space.";
+  exit 1
+fi
+
+# Install tools for dig and systemctl
+apt-get install git dnsutils systemd -y > /dev/null 2>&1
+
+# Check for systemd
+systemctl --version >/dev/null 2>&1 || { echo "systemd is required. Are you using Ubuntu 16.04?"  >&2; exit 1; }
+
+# CHARS is used for the loading animation further down.
+CHARS="/-\|"
+EXTERNALIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+
 USER=root
 USERHOME=`eval echo "~$USER"`
 
-echo "Shutting down masternode..."
-if [ -e /etc/systemd/system/fantasygoldd.service ]; then
-  systemctl stop fantasygoldd
-else
-  su -c "fantasygold-cli stop" $USER
+printHead0 "ASKING FOR INFORMATION"
+printHead1 "Please Confirm IP and PK [hit enter when confirming]"
+
+read -e -p "Confirm Server IP Address: " -i $EXTERNALIP -e IP
+read -e -p "Enter your Masternode Private Key : " KEY
+
+
+# Generate random passwords
+RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+# update packages and upgrade Ubuntu
+printHead0 "INSTALLING DEPENDENCIES"
+sleep 0.5
+printHead1 "updating system"
+sleep 0.5
+# Update the system.
+DEBIAN_FRONTEND=noninteractive apt-get install -yq libc6 software-properties-common
+DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc
+#DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
+#apt-get -f install -y
+DEBIAN_FRONTEND=noninteractive apt-get -yq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+
+
+echo
+apt-get -f install -y &
+waitOnProgram "Updating system. This may take several minutes"
+
+printHead1 "installing bitcoin"
+sleep 0.5
+echo | add-apt-repository ppa:bitcoin/bitcoin
+apt-get update
+apt-get install -y libdb4.8-dev libdb4.8++-dev
+
+# Add in older boost files if needed.
+printHead1 "installing boost"
+sleep 0.5
+if [ ! -f /usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0 ]; then
+  # Add in 16.04 repo.
+  echo "deb http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted" >> /etc/apt/sources.list
+  apt-get update -y
+
+  # Install old boost files.
+  apt-get install -y libboost-system1.58.0 libboost-filesystem1.58.0 libboost-program-options1.58.0 libboost-thread1.58.0
 fi
 
-echo "Upgrading fantasygold..."
-#mkdir ./fantasygold-temp #&& cd ./fantasygold-temp
+printHead1 "installing apps"
+sleep 0.5
+# Make sure certain programs are installed.
+apt-get install -y screen curl htop gpw unattended-upgrades jq bc pwgen libminiupnpc10 ufw lsof util-linux gzip denyhosts procps unzip
+
+printHead1 "writing auto update config"
+sleep 0.5
+if [ ! -f /etc/apt/apt.conf.d/20auto-upgrades ]; then
+  # Enable auto updating of Ubuntu security packages.
+  printf 'APT::Periodic::Enable "1";
+  APT::Periodic::Download-Upgradeable-Packages "1";
+  APT::Periodic::Update-Package-Lists "1";
+  APT::Periodic::Unattended-Upgrade "1";
+  APT::Get::Assume-Yes "true";
+  ' > /etc/apt/apt.conf.d/20auto-upgrades
+fi
+
+echo
+unattended-upgrade &
+waitOnProgram  "upgrading software, please wait some time"
+#sleep 0.5
+
+# Update system clock.
+timedatectl set-ntp off
+timedatectl set-ntp on
+
+printHead1 "installing fail2ban"
+sleep 0.5
+apt-get -qq install aptitude
+aptitude -y -q install fail2ban
+service fail2ban restart
+
+printHead1 "configuring firewall"
+sleep 0.5
+apt-get -qq install ufw
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 57810/tcp
+yes | ufw enable
+
+
+
+printHead0 "INSTALLING FGC"
+sleep 1
+# Install FantasyGold daemon
 wget $TARBALLURL
 tar -xzvf $TARBALLNAME #&& mv bin fantasygold-$FGCVERSION
 rm $TARBALLNAME
+cp ./fantasygoldd /usr/local/bin
+cp ./fantasygold-cli /usr/local/bin
+cp ./fantasygold-tx /usr/local/bin
+cp ./fantasygold-qt /usr/local/bin
+#rm -rf fantasygold-$FGCVERSION
 
-cp -f ./fantasygoldd /usr/local/bin
-cp -f ./fantasygold-cli /usr/local/bin
-cp -f ./fantasygold-tx /usr/local/bin
-rm test*
-rm fan*-qt
-#cd ..
-#rm -rf ./fantasygold-temp
+# Create .fantasygold directory
+mkdir $USERHOME/.fantasygold
 
-#if [ -e /usr/bin/fantasygoldd ];then rm -rf /usr/bin/fantasygoldd; fi
-#if [ -e /usr/bin/fantasygold-cli ];then rm -rf /usr/bin/fantasygold-cli; fi
-#if [ -e /usr/bin/fantasygold-tx ];then rm -rf /usr/bin/fantasygold-tx; fi
+printHead0 "INSTALLING BOOTSTRAP"
+sleep 1
 
-sed -i '/^addnode/d' ./.fantasygold/fantasygold.conf
-chmod 0600 ./.fantasygold/fantasygold.conf
-#chown -R $USER:$USER ./.fantasygold
+wget $BOOTSTRAPURL
+tar -xzf $BOOTSTRAPFILE -C $USERHOME/.fantasygold
+rm $BOOTSTRAPFILE
 
-echo "Restarting fantasygold daemon..."
-if [ -e /etc/systemd/system/fantasygoldd.service ]; then
-  systemctl start fantasygoldd
-else
-  cat > /etc/systemd/system/fantasygoldd.service << EOL
+
+printHead0 "CREATING CONFIGS"
+
+# Create fantasygold.conf
+touch $USERHOME/.fantasygold/fantasygold.conf
+cat > $USERHOME/.fantasygold/fantasygold.conf << EOL
+rpcuser=${RPCUSER}
+rpcpassword=${RPCPASSWORD}
+rpcallowip=127.0.0.1
+listen=1
+server=1
+daemon=1
+logtimestamps=1
+maxconnections=256
+externalip=${IP}
+bind=${IP}:57810
+masternodeaddr=${IP}
+masternodeprivkey=${KEY}
+masternode=1
+addnode=139.162.190.155
+addnode=176.58.126.105
+addnode=45.79.151.214
+addnode=45.79.203.106
+addnode=45.33.115.240
+addnode=45.33.49.18
+EOL
+chmod 0600 $USERHOME/.fantasygold/fantasygold.conf
+chown -R $USER:$USER $USERHOME/.fantasygold
+
+sleep 1
+
+cat > /etc/systemd/system/fantasygoldd.service << EOL
 [Unit]
 Description=fantasygoldd
 After=network.target
@@ -76,19 +234,38 @@ Restart=on-abort
 [Install]
 WantedBy=multi-user.target
 EOL
-  sudo systemctl enable fantasygoldd
-  sudo systemctl start fantasygoldd.service
-fi
 
-sleep 4
+printHead0 "STARTING FGC"
+sudo systemctl enable fantasygoldd
+sudo systemctl start fantasygoldd
+sudo systemctl start fantasygoldd.service
+printHead0 "WAITING 30 seconds for wallet loading"
+sleep 3
+printHead1 "Hi, Welcome to the FGC Community"
+sleep 3
+printHead1 "Check out our discord for the latest updates https://discord.gg/dGr9kU9"
+sleep 3
+printHead1 "Check out https://fantasygold.io"
+sleep 3
+printHead1 "back to waiting :) .. not long more..."
+sleep 3
+printHead1 "thanks again, looking forward to sharing our successes with your..."
+printHead1 "The FGC Team"
+sleep 15
 
+#clear
+
+#clear
+#echo "Your masternode is syncing. Please wait for this process to finish."
+
+printHead0 "BEGINING SYNC"
 until su -c "fantasygold-cli startmasternode local false 2>/dev/null | grep 'successfully started' > /dev/null" $USER; do
   for (( i=0; i<${#CHARS}; i++ )); do
     sleep 5
     #echo -en "${CHARS:$i:1}" "\r"
     clear
-    echo "Service Started. Your masternode is syncing. 
-    When Current = Synced then select your MN in the local wallet and start it. 
+    echo "Service Started. Your masternode is syncing.
+    When Current = Synced then select your MN in the local wallet and start it.
     Script should auto finish here."
     echo "
     Current Block: "
@@ -99,6 +276,21 @@ until su -c "fantasygold-cli startmasternode local false 2>/dev/null | grep 'suc
   done
 done
 
-su -c "/usr/lcoal/bin/fantasygold-cli getinfo" $USER
+#echo "Your masternode is syncing. Please wait for this process to finish."
+#echo "CTRL+C to exit the masternode sync once you see the MN ENABLED in your local wallet." && echo ""
+
+#until su -c "fantasygold-cli startmasternode local false 2>/dev/null | grep 'successfully started' > /dev/null" $USER; do
+#  for (( i=0; i<${#CHARS}; i++ )); do
+#    sleep 2
+#    echo -en "${CHARS:$i:1}" "\r"
+#  done
+#done
+
+sleep 1
+su -c "/usr/local/bin/fantasygold-cli startmasternode local false" $USER
+sleep 1
+clear
 su -c "/usr/local/bin/fantasygold-cli masternode status" $USER
+sleep 5
+
 echo "" && echo "Masternode setup completed." && echo ""
